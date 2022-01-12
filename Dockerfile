c28f1cbb700cf480208533da6216f66981480121
hpache <shenryp78@gmail.com>
2022-01-11 19:06:43 -0500

    Pushing initial files

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt-get update
+RUN apt-get install -y apache2
+RUN apt-get install -y apache2-utils
+RUN apt-get clean
+RUN apt install -y php
+RUN apt install -y libapache2-mod-php
+COPY index.php /var/www/html
+EXPOSE 80 
+CMD ["apache2ctl", "-D", "FOREGROUND"]
---
+++ index.php
@@ -0,0 +1,26 @@
+<!Doctype html>
+
+<html lang="EN">
+
+    <head>
+        <title> Test </title>
+    </head>
+
+    <body>
+
+        <h1> Hello World! </h1>
+
+        <?php
+
+            for ($i = 0; $i <= 10; $i++){ ?>
+
+                My Favorite Number is <?= $i ?>
+            <?php
+            }
+            ?>
+        
+    </body>
+
+</html>
+
+
